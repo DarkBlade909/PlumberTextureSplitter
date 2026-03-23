a16f613fb17e5de312816dfbebc044f0ea95993c
DarkBlade909 <52094720+DarkBlade909@users.noreply.github.com>
2026-03-23 18:03:41 -0400

    more texture name checks

--- _PlumberNormalConvert.py
+++ _PlumberNormalConvert.py
@@ -8,7 +8,42 @@ if __name__ == "__main__":
 	texcount = 0
 	try:
 		for file in os.listdir():
-		    if file.endswith("_normal.tga") or file.endswith("_nmap.tga") or file.endswith("_n.tga") or file.endswith("_nm.tga") or file.endswith("_norm.tga") or file.endswith("_normals.tga") or file.endswith("_nrm.tga") or file.endswith("_normal.vtf.tga") or file.endswith("_nmap.vtf.tga") or file.endswith("_n.vtf.tga") or file.endswith("_nm.vtf.tga") or file.endswith("_norm.vtf.tga") or file.endswith("_normals.vtf.tga") or file.endswith("_nrm.vtf.tga"):
+		    if file.endswith("_normal.tga") \
+		    or file.endswith("_nmap.tga") \
+		    or file.endswith("_n.tga") \
+		    or file.endswith("_nm.tga") \
+		    or file.endswith("_norm.tga") \
+		    or file.endswith("_normals.tga") \
+		    or file.endswith("_nrm.tga") \
+		    or file.endswith("_normal.vtf.tga") \
+		    or file.endswith("_nmap.vtf.tga") \
+		    or file.endswith("_n.vtf.tga") \
+		    or file.endswith("_nm.vtf.tga") \
+		    or file.endswith("_norm.vtf.tga") \
+		    or file.endswith("_normals.vtf.tga") \
+		    or file.endswith("_nrm.vtf.tga") \
+		    or file.endswith("_bump.tga") \
+		    or file.endswith("_b.tga") \
+		    or file.endswith("_bump.vtf.tga") \
+		    or file.endswith("_b.vtf.tga") \
+		    or file.endswith("_normal.png") \
+		    or file.endswith("_nmap.png") \
+		    or file.endswith("_n.png") \
+		    or file.endswith("_nm.png") \
+		    or file.endswith("_norm.png") \
+		    or file.endswith("_normals.png") \
+		    or file.endswith("_nrm.png") \
+		    or file.endswith("_normal.vtf.png") \
+		    or file.endswith("_nmap.vtf.png") \
+		    or file.endswith("_n.vtf.png") \
+		    or file.endswith("_nm.vtf.png") \
+		    or file.endswith("_norm.vtf.png") \
+		    or file.endswith("_normals.vtf.png") \
+		    or file.endswith("_nrm.vtf.png") \
+		    or file.endswith("_bump.png") \
+		    or file.endswith("_b.png") \
+		    or file.endswith("_bump.vtf.png") \
+		    or file.endswith("_b.vtf.png"):	
 		        image = Image.open(file, "r")
 		        channels = image.split()
 		        if len(channels) == 4:
